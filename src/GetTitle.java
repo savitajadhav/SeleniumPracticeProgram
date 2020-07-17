685146133c9c90bb24111bf746a8549f97936656
savitajadhav <52745129+savitajadhav@users.noreply.github.com>
2020-07-17 12:41:39 +0530

    Update GetTitle.java

--- src/GetTitle.java
+++ src/GetTitle.java
@@ -9,9 +9,8 @@ public class GetTitle {
 
 		System.setProperty("webdriver.chrome.driver","C:\\Users\\savita.jadhav\\Downloads\\chromedriver_win32\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-		//driver.get("https://www.google.com");
+	driver.get("https://www.google.com");
         //driver.navigate().to("https://www.google.com");
-        driver.get("https://aws-dev-1be627-c.adweb.com.au");
 		//System.out.println("The tilte of the Page:" + driver.getTitle());
         //Find the title of the element without using getTitle() 
        WebElement titleText = driver.findElement(By.tagName("title"));
