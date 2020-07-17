1d491ddceadc75ec0d161f43fd0c5facf31d40c1
savitajadhav <52745129+savitajadhav@users.noreply.github.com>
2020-07-17 12:40:55 +0530

    Update RetrieveTitlewitoutGetTitleMethod.java

--- src/RetrieveTitlewitoutGetTitleMethod.java
+++ src/RetrieveTitlewitoutGetTitleMethod.java
@@ -6,7 +6,7 @@ public class RetrieveTitlewitoutGetTitleMethod {
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver","C:\\Users\\savita.jadhav\\Downloads\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-		driver.get("https://aws-dev-1be627.adweb.com.au");
+		driver.get("https://rest-web-test.com");
 		
 	}
 
